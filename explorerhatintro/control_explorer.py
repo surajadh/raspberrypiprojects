33875aa90311fd3c4a7a429bcad26d9c9a8c4eb2
Suraj Adhikari <suraj.adhikari@kroger.com>
2017-09-16 00:16:03 -0400

    Added templates

--- explorerhatintro/control_explorer.py
+++ explorerhatintro/control_explorer.py
@@ -1,49 +1,55 @@
 from flask import Flask
-from flask import make_response
+from flask import render_template
 
 import RPi.GPIO as GPIO
 
 YELLOW = 17
-GREEN = 0       //TODO change this
-RED = 0         //TODO change this
+GREEN = 4
+RED = 27
 
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(YELLOW, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(RED, GPIO.OUT)
 
 app = Flask(__name__)
 
-@app.route("/on/<light>")
-def turnon(light):
-        if not light or light.isspace()
-                return make_response('No Light specified')
+@app.route("/")
+@app.route("/<light>/on/")
+def turnon(light = None):
+        if not light:
+                return render_template('explorer_button_template.html', message = 'No Light specified')
         if light == 'yellow':
                 GPIO.output(YELLOW, 1)
-                return make_response(light + ' is on')
+                return render_template('explorer_button_template.html', message = light + ' is on')
         elif light == 'red':
                 GPIO.output(RED, 1)
-                return make_response(light + ' is on')
+                return render_template('explorer_button_template.html', message = light + ' is on')
         elif light == 'green':
                 GPIO.output(GREEN, 1)
-                return make_response(light + ' is on')
+                return render_template('explorer_button_template.html', message = light + ' is on')
         else:
-                return make_response('Selected Light is not available in explorer pro')
+                return render_template('explorer_button_template.html', \
+        message = 'Selected Light is not available in explorer pro')
             
 
-@app.route("/off/<light>")
+@app.route("/<light>/off/")
 def turnoff(light):
-        if not light or light.isspace()
-                return make_response('No Light specified')
+        if not light or light.isspace():
+                return render_template('explorer_button_template.html', message = 'No Light specified')
         if light == 'yellow':
                 GPIO.output(YELLOW, 0)
-                return make_response(light + ' is off')
+                return render_template('explorer_button_template.html', message = light + ' is off')
         elif light == 'red':
                 GPIO.output(RED, 0)
-                return make_response(light + ' is off')
+                return render_template('explorer_button_template.html', message = light + ' is off')
         elif light == 'green':
                 GPIO.output(GREEN, 0)
-                return make_response(light + ' is off')
+                return render_template('explorer_button_template.html', message = light + ' is off')
         else:
-                return make_response('Selected Light is not available in explorer pro')
+                return render_template('explorer_button_template.html', \
+        message = 'Selected Light is not available in explorer pro')
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=80, debug = True)
